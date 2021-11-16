5f3ceb3bc4fc9855bffc63c4dffcc766de1440d8
Luiz Break <luiz.break@gmail.com>
2021-11-16 18:26:44 -0400

    Implemented sersalud API

---
+++ src/sersalud/getAllNominaEntries.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const AWS = require('aws-sdk'); 
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+    TableName: "sersalud-nomina-bkp-DEV",
+};
+
+module.exports.getAllNominaEntries = (event, context, callback) => {
+  // fetch all trasactions in this table
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the nomina records.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+    callback(null, response);
+  });
+};
